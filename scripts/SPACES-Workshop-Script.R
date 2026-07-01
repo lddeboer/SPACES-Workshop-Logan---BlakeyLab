9c2ab417d2cd5c6a8318546e204755da4c966fd3
Harper Easley <harper.easley@gmail.com>
2026-07-01 14:48:30 -0700

    added ridges plot and library

--- scripts/SPACES-Workshop-Script.R
+++ scripts/SPACES-Workshop-Script.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(ratdat)
+library(ggridges)
 
 ggplot(complete_old, aes(
   x = hindfoot_length,
@@ -11,5 +12,9 @@ myplot <- ggplot(complete_old, aes(
   y = weight))+
   geom_point(aes(color = "red"))
 
+ggplot(complete_old, aes(x = weight, y = species, fill = species)) +
+  geom_density_ridges() +
+  theme_ridges() +
+  theme(legend.position = "none")
 #color = red awesome 
   myplot
